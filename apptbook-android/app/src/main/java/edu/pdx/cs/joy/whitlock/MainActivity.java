0f1ab6fead4163ac32ac03ff45a3a8d3b4774a48
David Whitlock <david.m.whitlock@gmail.com>
2025-08-04 20:10:05 -0700

    Persist the sums to a file.

--- apptbook-android/app/src/main/java/edu/pdx/cs/joy/whitlock/MainActivity.java
+++ apptbook-android/app/src/main/java/edu/pdx/cs/joy/whitlock/MainActivity.java
@@ -8,12 +8,20 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,6 +46,12 @@ public class MainActivity extends AppCompatActivity {
 
         ListView sumsView = findViewById(R.id.sums);
         sumsView.setAdapter(this.sums);
+
+        try {
+            readSumsFromFile();
+        } catch (IOException e) {
+            Toast.makeText(this, "While reading sums file " + e, Toast.LENGTH_LONG).show();
+        }
     }
 
     public void showCalculator(View view) {
@@ -55,8 +69,45 @@ public class MainActivity extends AppCompatActivity {
                 if (data != null) {
                     int sum = data.getIntExtra(CalculatorActivity.SUM_VALUE, 0);
                     this.sums.add(sum);
+                    try {
+                        writeSumsToFile();
+                    } catch (IOException e) {
+                        Toast.makeText(this, "While writing sums file " + e, Toast.LENGTH_LONG).show();
+                    }
                 }
             }
         }
     }
+
+    private void writeSumsToFile() throws IOException {
+        File sumsFile = getSumsFile();
+        try (PrintWriter pw = new PrintWriter(new FileWriter(sumsFile))) {
+            for (int i = 0; i < this.sums.getCount(); i++) {
+                int sum = this.sums.getItem(i);
+                pw.println(sum);
+            }
+        }
+    }
+
+    @NonNull
+    private File getSumsFile() {
+        File dataDir = this.getDataDir();
+        return new File(dataDir, "sums.txt");
+    }
+
+    private void readSumsFromFile() throws IOException {
+        File sumsFile = getSumsFile();
+        if (!sumsFile.exists()) {
+            return;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(sumsFile))) {
+            for (String line = br.readLine(); line != null; line = br.readLine()) {
+                int sum = Integer.parseInt(line);
+                this.sums.add(sum);
+            }
+        }
+    }
+
+
 }
