6881b06471028622cae981432731fa45339c1d34
David Whitlock <david.m.whitlock@gmail.com>
2025-08-04 19:58:45 -0700

    Show the sums from the Calculator in a ListView.

--- apptbook-android/app/src/main/java/edu/pdx/cs/joy/whitlock/MainActivity.java
+++ apptbook-android/app/src/main/java/edu/pdx/cs/joy/whitlock/MainActivity.java
@@ -3,6 +3,8 @@ package edu.pdx.cs.joy.whitlock;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private static final int GET_SUM = 42;
     static final String CURRENT_TIME = "CURRENT TIME";
 
+    private ArrayAdapter<Integer> sums;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        this.sums = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+        ListView sumsView = findViewById(R.id.sums);
+        sumsView.setAdapter(this.sums);
     }
 
     public void showCalculator(View view) {
@@ -45,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 if (data != null) {
                     int sum = data.getIntExtra(CalculatorActivity.SUM_VALUE, 0);
-                    Toast.makeText(this, "Sum was: " + sum, Toast.LENGTH_LONG).show();
+                    this.sums.add(sum);
                 }
             }
         }
